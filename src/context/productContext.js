fef6552095f341933ac3c16e19661495f4d7f683
LUZ <balon.basket.90@gmail.com>
2022-03-01 19:38:15 -0600

    [+]
    productContext

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -1,9 +1,25 @@
-import React, { createContext } from "react";
-
+import React, { createContext,useEffect,useState } from "react";
+import {getFetchData} from "../utils/api";
 
 export const productContext = createContext();
 
-export const productProvider =({children})=>{
+export const productProvider =(props)=>{
+
+    const [productsData, setProductsData] = useState ([
+      { name:'',
+        _id='',
+        cost:0,
+        category:'',
+        img:{hdUrl:'',
+             url:''
+            }
+      }
+    ]);
+
+    useEffect(()=>{
+        getFetchData();
+    });
+
     return(
         <productContext.Provider>
               {children}
