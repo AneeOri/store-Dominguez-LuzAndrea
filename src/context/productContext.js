0e6fe9c8212d9d6593088404bf93d28df360782b
LUZ <balon.basket.90@gmail.com>
2022-03-02 19:03:45 -0600

    [+]
    apiProducts

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -1,9 +1,12 @@
 import React, { createContext,useEffect,useState } from "react";
 import {getFetchData} from "../utils/api";
+import { API_URL, RESOURCES, headers } from "../utils/constants";
 
 export const productContext = createContext();
 
 export const productProvider =(props)=>{
+
+    const url = `${API_URL}${RESOURCES.PRODUCTS}`;
 
     const [productsData, setProductsData] = useState ([
       { name:'',
@@ -17,12 +20,12 @@ export const productProvider =(props)=>{
     ]);
 
     useEffect(()=>{
-        getFetchData();
+        getFetchData(url, { headers,setProductsData});
     });
 
     return(
-        <productContext.Provider>
-              {children}
+        <productContext.Provider value={[productsData, setProductsData]}>
+              {props.children}
         </productContext.Provider>
     )
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
-  export const getFetchData = async (url,{ headers ,list,setList}) => {
+ export const getFetchData = async (url,{ headers ,setProductsData}) => {
 	try {
 		const result = await fetch(url, { headers });
 		const products = await result.json();
-		setList(products);
-        console.log("data fetched:", list);
-        return list;
+		setProductsData(products);
+        console.log("data fetched:", products);
+        //return list;
 	} catch (error) {
 		console.log('error', error);
 	}
