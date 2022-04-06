90006230323c0f347d5ea95a867f6637d56b0a12
LUZ <balon.basket.90@gmail.com>
2022-04-06 18:29:16 -0500

    [+] Header
    contexto de users

---
+++ src/components/header/navBar.js
@@ -0,0 +1,16 @@
+import React,{useContext, UserContext} from 'react';
+import { UserContext } from '../../context/userContext';
+import coin from '../../assets/icons/coin.svg'
+
+export default function NavBar (){
+
+    const [userData] = useContext(UserContext);
+
+    return(
+        <div>
+          <p>{userData.name}</p>
+          <img src={coin} alt="imagen de moneda" />
+          <p>{userData.points}</p>
+        </div>
+    );
+}
