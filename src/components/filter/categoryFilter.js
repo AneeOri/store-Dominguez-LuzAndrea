15982a40a6571a219646ee16580c0650015257cc
LUZ <balon.basket.90@gmail.com>
2022-04-07 19:54:34 -0500

    [+] filtersCategory
    category and price filters

---
+++ src/components/filter/categoryFilter.js
@@ -0,0 +1,25 @@
+import React , {useState, useContext} from 'react';
+import { FilterContext } from '../../context/filterContext';
+import { ProductContext } from '../../context/productContext';
+
+export default function CategoryFilter(){
+
+    const [productData]=useContext(ProductContext);
+    const [filter, setFilter]= useContext(FilterContext);
+
+    const category = [
+        ...new Set(productData.map((product)=> product.category))
+    ].sort();
+
+    return (
+      <div>
+         <div>Filter by Category</div>
+         <select value={genero} onChange={onChangeGenero}>
+           <option>All</option>  
+           {category.map((category) => (
+                <option key={category} value={category.toLowerCase()}>{category}</option>
+           ))}
+         </select>
+      </div>  
+    );
+}
---
+++ src/components/filter/pricesFilter.js
@@ -0,0 +1,11 @@
+import React , {useState, useContext} from 'react';
+import { FilterContext } from '../../context/filterContext';
+import { ProductContext } from '../../context/productContext';
+
+export default function PricesFilter(){
+    return (
+      <div>
+          
+      </div>
+    );
+}
