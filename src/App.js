8e01e08c4edca4a443b222fcd9730321ccb77eb8
LUZ <balon.basket.90@gmail.com>
2022-04-09 23:10:30 -0500

    [+] filter options and context
    filter options and context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ProductProvider } from "./context/productContext";
 import { UserProvider } from "./context/userContext";
+import { FilterProvider } from "./context/filterContext";
 import Feed from "./components/feed/feed";
 import Header from "./components/header/header";
 
@@ -9,10 +10,12 @@ export default function App() {
   return (
     <UserProvider>
        <ProductProvider>
-          <div>
-            <Header/>
-            <Feed/>
-           </div>
+         <FilterProvider>
+            <div>
+              <Header/>
+              <Feed/>
+             </div>
+          </FilterProvider>   
        </ProductProvider>
     </UserProvider>   
   );
--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -1,11 +1,14 @@
 import React, {useContext, useEffect, useState} from "react";
 import { ProductContext } from "../../context/productContext";
 import Item from "../item/item/item";
+import CategoryFilter from "../filter/categoryFilter";
+import PricesFilter from "../filter/pricesFilter";
 
 export default function Feed (){
   
     const [productsData]=useContext(ProductContext);
     const [products,setProducts]=useState([]);
+   
 
     useEffect(()=>{
         setProducts([...productsData]);
@@ -14,6 +17,10 @@ export default function Feed (){
 
     return(
         <div >
+            <div>
+                <CategoryFilter/>
+                <PricesFilter/>
+            </div>
            {products.map((product) => (
 					<Item {...product} key={product._id} />
 				))}
