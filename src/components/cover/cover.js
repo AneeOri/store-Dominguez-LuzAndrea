eeb631be3823fbecf7c23b42e060d7fff8567a26
LUZ <balon.basket.90@gmail.com>
2022-04-10 23:43:28 -0500

    [+] cover styles
    cover styles

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import coverImage from '../../assets/header-x1.png';
+import "./cover.css";
 
 export default function Cover (){
     return(
-        <div>
+        <div className="coverImage">
              <img src={coverImage} alt="cover image" /> 
              <h1>Rewards Store</h1>
         </div>
