41a1c488a341f0f84774d3e6d655b4bb5bfa6106
LUZ <balon.basket.90@gmail.com>
2022-04-11 19:37:00 -0500

    [+] links header
    links header

--- src/components/header/navBar.js
+++ src/components/header/navBar.js
@@ -1,6 +1,7 @@
 import React,{useContext} from 'react';
 import { UserContext } from '../../context/userContext';
-import coin from '../../assets/icons/coin.svg'
+import coin from '../../assets/icons/coin.svg';
+import { Link } from "react-router-dom";
 
 export default function NavBar (){
 
@@ -11,6 +12,19 @@ export default function NavBar (){
           <p>{userData.name}</p>
           <img src={coin} alt="imagen de moneda" />
           <p>{userData.points}</p>
+          <nav>
+             <ul >
+               <Link to="/points">
+                 <li >Get Coins</li>
+               </Link>
+               <Link to="/history">
+                  <li >Reddems</li>
+                </Link>
+                <Link to="/">
+                   <li >Shop</li>
+                </Link>
+             </ul>
+          </nav>
         </div>
     );
 }
