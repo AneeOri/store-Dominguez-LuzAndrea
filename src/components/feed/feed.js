e902a24b8e51e86b8153c8652ec0c5e6e6d02041
LUZ <balon.basket.90@gmail.com>
2022-04-12 21:07:47 -0500

    [+] pagination completed
    pagination completed

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -4,13 +4,14 @@ import { FilterContext } from "../../context/filterContext";
 import Item from "../item/item/item";
 import CategoryFilter from "../filter/categoryFilter";
 import PricesFilter from "../filter/pricesFilter";
+import ReactPaginate from "react-paginate";
 
 export default function Feed (){
   
     const [productsData]=useContext(ProductContext);
     const [products,setProducts]=useState([]);
     const [filteredData] = useContext(FilterContext);
-   
+
 
     useEffect(()=>{
         setProducts([...productsData]);
@@ -47,16 +48,37 @@ export default function Feed (){
             }
         }
     }
+    const [currentPage, setCurrentPage] = useState(0);
+    const ITEMS_PER_PAGE =16;
+    const count = Math.ceil(products.length / ITEMS_PER_PAGE);
+    const pagesVisied = currentPage *ITEMS_PER_PAGE;
+
+    const displayProducts = products
+    .slice(pagesVisied, pagesVisied+ITEMS_PER_PAGE)
+    .map(product=> (
+        <Item {...product} key={product._id} />
+    ));
+
+    const changePage= ({selected}) => {
+        setCurrentPage(selected);
+    };
 
     return(
-        <div >
+        <div>
             <div>
-                <CategoryFilter/>
-                <PricesFilter/>
+              <CategoryFilter/>
+              <PricesFilter/>
             </div>
-           {products.map((product) => (
-					<Item {...product} key={product._id} />
-				))}
-         </div>
+            <div>
+               <ReactPaginate
+                  breakLabel="..."
+                  nextLabel="next >"
+                  onPageChange={changePage}
+                  pageCount={count}
+                  previousLabel="< previous"
+               />
+            </div>
+              {displayProducts}
+        </div>
     );
 }
