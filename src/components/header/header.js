8f51da19bb3f62e1028d8f53a70c3d50684e2982
LUZ <balon.basket.90@gmail.com>
2022-04-17 16:37:40 -0500

    [+] header directions and styles
    header directions and styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "../../assets/aerolab-logo.svg";
 import NavBar from "./navBar";
+import "./header.css";
 
 
 export default function Header (){
 
     return(
-        <div height="100px" width="100px">
+        <div height="100px" width="100px" className="header">
            <img src={logo} alt="Logo de Aerolab" />
            <NavBar/>
         </div>
--- src/components/header/navBar.js
+++ src/components/header/navBar.js
@@ -2,31 +2,36 @@ import React,{useContext} from 'react';
 import { UserContext } from '../../context/userContext';
 import coin from '../../assets/icons/coin.svg';
 import { Link } from "react-router-dom";
+import "./navBar.css";
 
 export default function NavBar (){
 
     const [userData] = useContext(UserContext);
 
     return(
-        <div>
-          <p>{userData.name}</p>
-             <div>
-             <p >{userData.points}</p>
-             <img src={coin} alt="imagen de moneda" />
-             </div>   
-          <nav>
-             <ul >
-               <Link to="/points">
-                 <li >Get Coins</li>
-               </Link>
-               <Link to="/history">
-                  <li >Reddems</li>
-                </Link>
-                <Link to="/">
-                   <li >Shop</li>
-                </Link>
-             </ul>
-          </nav>
+        <div className="navBar">
+             <div className='points'>
+                  <p>{userData.name}</p>
+                  <div className='name'>
+                     <p >{userData.points}</p>
+                     <img src={coin} alt="imagen de moneda" />
+                  </div>
+             </div>  
+             <div >
+                  <nav>
+                    <ul className='links'>
+                      <Link to="/points">
+                        <li >Get Coins</li>
+                       </Link>
+                       <Link to="/history">
+                           <li >Reddems</li>
+                        </Link>
+                        <Link to="/">
+                           <li >Shop</li>
+                        </Link>
+                    </ul>
+                  </nav>
+            </div> 
         </div>
     );
 }
