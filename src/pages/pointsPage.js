58b642c58d72e2b5d14f1db91e0f85030c3639c8
LUZ <balon.basket.90@gmail.com>
2022-04-19 22:12:55 -0500

    [+]
    getting more points

--- src/pages/pointsPage.js
+++ src/pages/pointsPage.js
@@ -3,6 +3,7 @@ import { UserContext } from '../context/userContext';
 import { postPoints } from '../utils/api';
 import { API_URL, RESOURCES, headers } from '../utils/constants';
 import coin from '../assets/icons/coin.svg';
+import "./points.css";
 
 
 export default function PointsPage() {
@@ -16,18 +17,18 @@ export default function PointsPage() {
    }
 
   return (
-    <div>
+    <div className='coinsContainer'>
       <h1>Get More Coins</h1>
       <h3>How many coins do you want to add?</h3>
-      <div>
+      <div className='coinButtons'>
         <button onClick={()=>{postingPoints(1000)}}> 1000
-           <img src={coin} alt="imagen de moneda" />
+           <img className='imgC' src={coin} alt="imagen de moneda" />
         </button>
         <button onClick={()=>{postingPoints(5000)}}> 5000
-           <img src={coin} alt="imagen de moneda" />
+           <img className='imgC' src={coin} alt="imagen de moneda" />
         </button>
         <button onClick={()=>{postingPoints(7500)}}> 7500
-           <img src={coin} alt="imagen de moneda" />
+           <img className='imgC' src={coin} alt="imagen de moneda" />
         </button>
       </div>
     </div>
