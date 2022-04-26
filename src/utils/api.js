5f040d35e54aa0c1739e04dde60b0604c1bf5bfe
LUZ <balon.basket.90@gmail.com>
2022-04-25 21:11:23 -0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,6 @@
 		const result = await fetch(url, { headers });
 		const products = await result.json();
 		setProductsData(products);
-        console.log("data fetched:", products);
-        //return list;
 	} catch (error) {
 		console.log('error', error);
 	}
@@ -15,8 +13,6 @@ export const getFetchUser = async ({ headers ,setUserData},url) => {
 		const result = await fetch(url, { headers });
 		const user = await result.json();
 		setUserData(user);
-        console.log("data fetched:", user);
-        //return list;
 	} catch (error) {
 		console.log('error', error);
 	}
@@ -27,7 +23,6 @@ export const getHistory = async ({headers,setHistoryData}, url) => {
 		const result = await fetch(url, { headers });
 		const history = await result.json();
 		setHistoryData(history);
-        console.log("data fetched:", history);
 	}catch(error){
 		console.log('error',error);
 	}
@@ -67,7 +62,6 @@ export const postPoints = async({amount,headers, userData, setUserData},url) => 
 		 const newPointsUser = {...userData};
 		 newPointsUser.points = newPoints['New Points'];
 		 setUserData(newPointsUser);
-		 console.log("data fetched:", newPointsUser);
 	}catch(error){
 		console.log('error',error);
 	}
